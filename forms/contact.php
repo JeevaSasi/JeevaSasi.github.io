45358ba9ba459d56718548687d2680c25b533ea4
JeevaTechbylanz <jeevasasikumar87@gmail.com>
2025-04-20 10:41:45 +0530

    email issue fixed
    email issue fixed

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'jeeva@techbylanz.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -15,27 +15,31 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
+  $contact = new $php_email_form;
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
+  $contact->phone = $_POST['phone'];
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'smartglobalsolutions25@gmail.com',
+    'password' => 'Sgs@2025',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['phone'], 'Phone');
+  $contact->add_message( $_POST['subject'], 'Subject');
+  $contact->add_message( $_POST['message'], 'Message');
+
 
   echo $contact->send();
 
@@ -58,6 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $phone = $_POST['phone'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
@@ -91,6 +96,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class='content'>
                 <p><strong>Name:</strong> $name</p>
                 <p><strong>Email:</strong> $email</p>
+                <p><strong>Phone:</strong> $phone</p>
                 <p><strong>Subject:</strong> $subject</p>
                 <p><strong>Message:</strong></p>
                 <p>$message</p>
